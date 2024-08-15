30d011091542f8e936c4c2859d2185b9219159bb
Fitzroyfang <msofivaz96@gmail.com>
2024-08-15 09:15:49 -0300

    agregado el sitio products y su funcionalidad

---
+++ js/products.js
@@ -0,0 +1,39 @@
+let categorias = []
+
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let category = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.name +`</h4> 
+                        <p> `+ category.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + category.productCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            showCategoriesList(categorias);
+        }
+    });
+});
