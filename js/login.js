04633676b894ea8f40fb82dc6e4ff8e2039d8902
Valentina <valyazrochizq@hotmail.com>
2024-10-18 12:37:51 -0300

    imagen de perfil

--- js/login.js
+++ js/login.js
@@ -3,7 +3,6 @@ function login () {
     let pass = document.getElementById('password').value;
 
 
-
     if (user != '' && pass != '') {
         sessionStorage.setItem("user", user);
         location.href = "index.html"
@@ -26,3 +25,43 @@ document.getElementById("password").addEventListener('keydown', function (e) {
         login();
     }
 })
+
+const imageInput = document.getElementById('imageInput');
+
+// Guardar imagen
+function saveImage(file) {
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      localStorage.setItem('savedImage', event.target.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  // Mostrar imagen
+
+  function displayImage() {
+    const savedImage = localStorage.getItem('savedImage');
+    if (savedImage) {
+      document.getElementById('imagePreview').src = savedImage; // Muestra la imagen
+      console.log("Imagen cargada desde localStorage", saveImage);
+    } else {
+      console.error("No se encontró la imagen en localStorage");
+    }
+  }
+
+  // Evento para cargar la imagen
+
+  imageInput.addEventListener('change', function(event) {
+    const selectedFile = event.target.files[0]; // Guarda la imagen seleccionada
+    if (selectedFile) {
+      saveImage(selectedFile);
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        document.getElementById('imagePreview').src = e.target.result; // Muestra una vista previa
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  });
+
+
+window.onload = displayImage;
