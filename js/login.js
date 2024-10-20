517b50477d545941442de2c232d72408845493c5
smoonlighth <sabrinahe204@gmail.com>
2024-10-19 21:35:52 -0300

    Perfil funcional

--- js/login.js
+++ js/login.js
@@ -1,19 +1,17 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('btn-inicio').addEventListener('click',()=>{
-        let user = document.getElementById('usuario').value;
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('btn-inicio').addEventListener('click', () => {
+        let emailField = document.getElementById('usuario');
         let pass = document.getElementById('password').value;
-        
 
-
-        if (user!='' && pass!='') {
-            sessionStorage.setItem("user", user);
-            location.href="index.html"
-        }else if (user==='' && pass!=''){
-            alert("Por favor ingrese usuario")
-        }else if (user!='' && pass===''){
-            alert("Por favor ingrese contraseña")
-        }else {
-            alert("Por favor ingrese usuario y contraseña")
+        if (emailField.checkValidity() && pass !== '') {
+            sessionStorage.setItem("user", emailField.value);
+            location.href = "index.html";
+        } else if (!emailField.checkValidity() && pass !== '') {
+            alert("Por favor ingrese un correo electrónico válido");
+        } else if (emailField.checkValidity() && pass === '') {
+            alert("Por favor ingrese su contraseña");
+        } else {
+            alert("Por favor ingrese su correo electrónico y contraseña");
         }
-    })
-})
+    });
+});
