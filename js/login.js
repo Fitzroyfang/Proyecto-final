0a0acca618101adf3c4bc5829c54a41ab15de5db
smoonlighth <sabrinahe204@gmail.com>
2024-10-19 22:16:27 -0300

    errores solucionados

--- js/login.js
+++ js/login.js
@@ -15,3 +15,54 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+document.getElementById('btn-inicio').addEventListener('click', login);
+
+
+document.getElementById("password").addEventListener('keydown', (e) => {
+    if (e.key === "Enter") {
+        login();
+    }
+});
+
+const imageInput = document.getElementById('imageInput');
+
+// Guardar imagen
+function saveImage(file) {
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      localStorage.setItem('savedImage', event.target.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  // Mostrar imagen
+
+  function displayImage() {
+    const savedImage = localStorage.getItem('savedImage');
+    if (savedImage) {
+      document.getElementById('imagePreview').src = savedImage; // Muestra la imagen
+      console.log("Imagen cargada desde localStorage", saveImage);
+    } else {
+      console.error("No se encontró la imagen en localStorage");
+    }
+  }
+
+  // Evento para cargar la imagen
+
+  imageInput.addEventListener('change', function(event) {
+    const selectedFile = event.target.files[0]; // Guarda la imagen seleccionada
+    if (selectedFile) {
+      saveImage(selectedFile);
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        document.getElementById('imagePreview').src = e.target.result; // Muestra una vista previa
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  });
+
+
+window.onload = displayImage;
+
